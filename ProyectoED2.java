0f780390e4828f7a38009d8dc4ea9d08905afb1a
Parker050 <149011442+Parker050@users.noreply.github.com>
2024-08-06 09:36:58 -0600

    Add files via upload

--- ProyectoED2.java
+++ ProyectoED2.java
@@ -1,29 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyectoed2;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+
 public class ProyectoED2 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        alanCode ruta = new alanCode();
-        ArrayList<Registro> busqueda = new ArrayList<>();
-        
-        //ruta.busqueda(busqueda);
-        //ruta.viajeEnL("2", "4", "6", "4");
-        //ruta.mismaLinea("2", "6", "4", true);
-        
-        //System.out.println(ruta.nodoCercano("1", "2"));
-        
+        try {
+            algoritmoRuta ruta = new algoritmoRuta();
+            ArrayList<Registro> busqueda = new ArrayList<>();
+
+            ruta.busqueda(busqueda);
+            //ruta.viajeEnL("2", "4", "6", "4");
+            //ruta.mismaLinea("2", "6", "4", true);
+            //System.out.println(ruta.nodoCercano("1", "2"));
+        } catch (Exception e) {
+            // Manejo de la excepción
+            System.err.println("Se ha producido un error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
